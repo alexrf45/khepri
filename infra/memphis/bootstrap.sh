28056e28e72fddab7bf3afe7d9845aa52ab3daa6
alexrf45 <fonalex45@gmail.com>
2025-11-02 23:08:49 -0500

    oops

--- infra/memphis/bootstrap.sh
+++ infra/memphis/bootstrap.sh
@@ -29,8 +29,8 @@ flux-deploy() {
   #
   flux bootstrap git \
     --cluster-domain=cluster.local \
-    --url=ssh://git@github.com/alexrf45/home-ops.git \
-    --path=clusters/testing \
+    --url=ssh://git@github.com/alexrf45/khepri.git \
+    --path=clusters/memphis \
     --private-key-file=/home/fr3d/.ssh/fr3d \
     --branch main \
     --force
